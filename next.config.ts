f43937d72d4c68109eedd380a001262b4659a5b4
ccl <>
2026-01-30 17:14:20 -0500

    - fix base path

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
+const basePath = process.env.GITHUB_ACTIONS ? "/smotown" : "";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? "/smotown" : "",
-  assetPrefix: isProd ? "/smotown/" : "",
+  basePath,
   images: {
     unoptimized: true,
   },
